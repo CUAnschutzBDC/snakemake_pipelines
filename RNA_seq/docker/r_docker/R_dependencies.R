d43619a498082a1db990cf2ca0367c59af70cc9d
kwells4 <kristen.wells@cuanschutz.edu>
2023-10-30 11:59:21 -0600

    updating RNA-seq pipeline

---
+++ RNA_seq/docker/r_docker/R_dependencies.R
@@ -0,0 +1,22 @@
+library(tidyverse)
+library(here)
+library(pheatmap)
+library(viridis)
+library(pathview)
+library(openxlsx)
+library(gprofiler2)
+library(knitr)
+library(markdown)
+library(LaCroixColoR) # johannesbjork/LaCroixColoR
+library(rmarkdown)
+library(MetBrewer) # BlakeRMills/MetBrewer
+library(inauguration) # ciannabp/inauguration
+library(DESeq2)
+library(ggrepel)
+library(cowplot)
+library(Matrix)
+library(RColorBrewer)
+library(ashr)
+library(apeglm)
+library(KEGGREST)
+library(org.Mm.eg.db)
